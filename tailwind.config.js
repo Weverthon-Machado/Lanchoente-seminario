d360f2270f6bdde9df27828196e94dd33f4d8519
WEVERTHON VAZ <weverthonmachado240@gmail.com>
2024-05-18 18:57:38 -0300

    lanchonete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Roboto', 'sans-serif']
+    },
+    extend: { backgroundImage: { "home": "url('/assest/bg.png')" }
+    },
+  },
+  plugins: [],
+}
+
